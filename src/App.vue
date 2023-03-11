143bbd95287500b7e493f7a9237828940af5502f
Maksim Zawadzki <zawadzki2778@gmail.com>
2023-03-11 16:54:33 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <div class="navbar">
         <div class="container">
           <div class="navbar-content">
-            <div class="logo">VUE-NOTIFY</div>
+            <div class="logo">VUE NOTIFY</div>
             <ul class="navbar-list">
               <li class="navbar-item" v-for="link in links" :key="link.title">
                 <router-link
